f236c3423c96428a18137b3e68795c796c85dfd0
ding <playdingnow@gmail.com>
2017-03-03 14:49:37 +0800

    feat: use end as exit

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const config = require('./config');
 const siteOperator = module.require(`./sites/${config.site}`);
 
 siteOperator.run()
+  .end()
   .then(result => process.stdout.write(`\n  result: \n${JSON.stringify(result)}`))
   .catch(error => process.stderr.write(`\n  error: \n${error}`));
