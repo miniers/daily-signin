ff4f5c4ddb368c5ed0f67c772d1b437d55295a61
ding <playdingnow@gmail.com>
2017-03-03 13:39:25 +0800

    fix: zsh env taken

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const config = {
   site: process.env.SITE || 'smzdm',
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
+  username: process.env.DUSERNAME,
+  password: process.env.DPASSWORD,
 };
 
 const siteOperator = module.require(`./sites/${config.site}`);
 
 siteOperator.run(config)
   .then(result => process.stdout.write(`\n  result: \n${JSON.stringify(result)}`))
-  .catch(error => process.stderr.write(`\n  error: \n${error.message}`));
+  .catch(error => process.stderr.write(`\n  error: \n${error}`));
