b28da993ade9cddb0d32a0179b45b1a0d3ff3c2f
ding <playdingnow@gmail.com>
2017-03-03 13:06:15 +0800

    feat(smzdm): pre login form click

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const config = {
-  site: process.env.SITE,
+  site: process.env.SITE || 'smzdm',
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
 };
 
 const siteOperator = module.require(`./sites/${config.site}`);
 
-siteOperator(config);
+siteOperator.run(config)
+  .then(result => process.stdout.write(`\n  result: \n${JSON.stringify(result)}`))
+  .catch(error => process.stderr.write(`\n  error: \n${error.message}`));
--- sites/smzdm.js
+++ sites/smzdm.js
@@ -1,9 +1,30 @@
-const axios = require('axios');
+const Nightmare = require('nightmare');
 
-const run = (config) => {
-  // const { username, password } = config;
-  axios.get(config);
+const nightmare = Nightmare({ show: true });
+const URLS = {
+  home: 'http://www.smzdm.com',
+  login: 'https://zhiyou.smzdm.com/user/login/ajax_check',
 };
 
+const ELES = {
+  login: '.J_login_trigger',
+  usernameInput: '#username',
+  passwordInput: '#password',
+};
 
-module.exports = run;
+const run = (config) => {
+  const { username, password } = config;
+
+  return nightmare
+    .goto(URLS.home)
+    .wait(ELES.login)
+    .click(ELES.login)
+    .wait(ELES.usernameInput)
+    .type(ELES.usernameInput, username)
+    .type(ELES.passwordInput, password);
+};
+
+module.exports = {
+  run,
+  URLS,
+};
