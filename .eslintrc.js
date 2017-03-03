52aebe44a369568a80a354339e310ab5c570dc46
ding <playdingnow@gmail.com>
2017-03-03 14:49:23 +0800

    chore: add document as global

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ]
+  .extends.: "airbnb-base",
+  plugins: [
+    "import",
+  ],
+  globals: {
+    document: true,
+  }
 };
