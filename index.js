27b8bae3f6fc08a32496c74cdc98d65b0630d1ae
ding <playdingnow@gmail.com>
2017-03-05 14:58:33 +0800

    feat: not catch error

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const siteOperator = module.require(`./sites/${config.site}`);
 
 siteOperator.run()
   .end()
-  .then(result => process.stdout.write(`\n  result: \n${JSON.stringify(result)}`))
-  .catch(error => process.stderr.write(`\n  error: \n${error}`));
+  .then(result => process.stdout.write(`\n  result: \n${JSON.stringify(result)}`));
