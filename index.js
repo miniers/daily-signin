01cece6918c0d92c51a9da44156c70b3119ec656
ding <playdingnow@gmail.com>
2017-03-13 08:50:19 +0800

    feat: print error detail

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+/* eslint no-console: ["error", { allow: ["error"] }]  */
 const config = require('./config');
 
 const siteOperator = module.require(`./sites/${config.site}`);
 
 siteOperator.run()
+  .catch((error) => {
+    console.error('CATCH ERROR: ', error);
+    process.exit(1);
+  })
   .then(result => process.stdout.write(`\n  result: \n${JSON.stringify(result)}`));
